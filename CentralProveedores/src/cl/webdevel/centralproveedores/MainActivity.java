f929ad61021954ebeeaedc5e15b0c61f761a82cf
Rodrigo Campos <rodrigo@webdevel.cl>
2014-12-29 03:08:03 -0300

    Agregado botones

--- CentralProveedores/src/cl/webdevel/centralproveedores/MainActivity.java
+++ CentralProveedores/src/cl/webdevel/centralproveedores/MainActivity.java
@@ -10,23 +10,64 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
-	Button mapaBtn;
+	Button mapa1Btn;
+	Button mapa2Btn;
+	Button mapa3Btn;
+	Button mapa4Btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mapaBtn = (Button) findViewById(R.id.mapaBtn);
+		mapa1Btn = (Button) findViewById(R.id.mapa1Btn);
+		mapa2Btn = (Button) findViewById(R.id.mapa2Btn);
+		mapa3Btn = (Button) findViewById(R.id.mapa3Btn);
+		mapa4Btn = (Button) findViewById(R.id.mapa4Btn);
 		
-		mapaBtn.setOnClickListener(new OnClickListener() {
+		mapa1Btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				Intent mapIntent = new Intent().setClass(MainActivity.this, MapActivity.class);
+				mapIntent.putExtra("TIPO", 0);
+				startActivity(mapIntent);
+				
+			}
+		});
+		
+		mapa2Btn.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
-				// Toast.makeText(MainActivity.this, "HOLA", Toast.LENGTH_SHORT).show();
-				
 				Intent mapIntent = new Intent().setClass(MainActivity.this, MapActivity.class);
+				mapIntent.putExtra("TIPO", 1);
+				startActivity(mapIntent);
+				
+			}
+		});
+		
+		mapa3Btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				Intent mapIntent = new Intent().setClass(MainActivity.this, MapActivity.class);
+				mapIntent.putExtra("TIPO", 2);
+				startActivity(mapIntent);
+				
+			}
+		});
+		
+		mapa4Btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				Intent mapIntent = new Intent().setClass(MainActivity.this, MapActivity.class);
+				mapIntent.putExtra("TIPO", 3);
 				startActivity(mapIntent);
 				
 			}
--- CentralProveedores/src/cl/webdevel/centralproveedores/MapActivity.java
+++ CentralProveedores/src/cl/webdevel/centralproveedores/MapActivity.java
@@ -11,11 +11,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+	
+	Integer tipo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		tipo = getIntent().getExtras().getInt("TIPO");
 		
 		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
@@ -25,41 +29,68 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 	@Override
 	public void onMapReady(GoogleMap map) {
 		
-		map.addMarker(new MarkerOptions()
-			.position(new LatLng(-36.828346, -73.045447))
-			.title("Inmobiliaria Aitue"));
+		switch(tipo) {
 		
-		map.addMarker(new MarkerOptions()
-			.position(new LatLng(-40.572635, -73.119883))
-			.title("Desarmaduria Nacional"));
-		
-		map.addMarker(new MarkerOptions()
-			.position(new LatLng(-33.452554, -70.781600))
-			.title("Mundo Repuestos"));
-		
-		map.addMarker(new MarkerOptions()
-			.position(new LatLng(-36.784130, -73.081659))
-			.title("Transver"));
-		
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(-33.460298, -70.629981))
-		.title("Full Diesel"));
-		
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(-36.826946, -73.057308))
-		.title("Adriazola Repuestos"));
+			case 0:
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.817828, -73.048915))
+				.title("MSA"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.823730, -73.042176))
+				.title("Kupfer"));
+				
+				break;
+				
+			case 1:
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.811196, -73.035832))
+				.title("Empresa Constructora Bellolio"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.827631, -73.049624))
+				.title("Constructora Torrepiedra"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.827889, -73.047345))
+				.title("Constructora el Parque"));
 
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(-36.716311, -73.114500))
-		.title("Securitas"));
+				break;
+				
+			case 2:
 
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(-36.817663, -73.057541))
-		.title("P y S Refrigeración"));
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.824707, -73.046181))
+				.title("Provin"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.825227, -73.042701))
+				.title("Aprivim"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.820213, -73.059738))
+				.title("MG Proveedores"));
+				
+				break;
+				
+			case 3:
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.827345, -73.052060))
+				.title("BSK Proveedores de insumos"));
+				
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(-36.826464, -73.048261))
+				.title("Indepot"));
+
+				break;
+				
+		}
 		
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(-33.482643, -70.677998))
-		.title("Bori Clean Service"));
+		
+
 		
 	    
 	}
